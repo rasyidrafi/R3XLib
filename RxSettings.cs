f25d8c6de56b6c7babf907e000936e12e5d3c0cd
Rasyid Rafi <rasyid.rafi@yahoo.com>
2022-10-13 18:23:58 +0700

    change name

--- RxSettings.cs
+++ RxSettings.cs
@@ -6,12 +6,12 @@ namespace R3xLib
 {
     public class RxSettings
     {
-        public static readonly string ModuleVer = "https://raw.githubusercontent.com/rasyidrafi/R3XLib/main/release/RxVer.txt";
-        public static readonly string Module = "https://raw.githubusercontent.com/rasyidrafi/R3XLib/main/release/RxDll.dll";
-        public static readonly string ModuleSha = "https://raw.githubusercontent.com/rasyidrafi/R3XLib/main/release/RxSha.txt";
+        public static readonly string ModuleVer = "https://raw.githubusercontent.com/rasyidrafi/R3XLib/main/output/RxVer.txt";
+        public static readonly string Module = "https://raw.githubusercontent.com/rasyidrafi/R3XLib/main/output/R3xLib.dll";
+        public static readonly string ModuleSha = "https://raw.githubusercontent.com/rasyidrafi/R3XLib/main/output/RxSha.txt";
         public static readonly string ModuleVerPath = "./RxVersion.txt";
-        public static readonly string ModulePath = "./RxDll.dll";
-        public static readonly string ModuleName = "RxDll.dll";
+        public static readonly string ModulePath = "./R3xLib.dll";
+        public static readonly string ModuleName = "R3xLib.dll";
         public static readonly string ModuleCode = "scriptwaresucks";
     }
 }
